fa70e17463ce1cf3cd0336e30ccce1701ce0895f
RubyQianru <qz2432@nyu.edu>
2024-11-16 20:42:17 -0500

    💥 Add VADER sentiment analysis

--- util/text.py
+++ util/text.py
@@ -1,21 +1,34 @@
 import re
 from flair.data import Sentence
 from flair.nn import Classifier
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
+# Flair tagger
 tagger = Classifier.load('sentiment')
+# VADER analyzer
+analyzer = SentimentIntensityAnalyzer()
+
 
 def extract_urls(input:str):
   url_pattern = r'https?://\S+'
   urls = re.findall(url_pattern, text)
   return urls[0] if urls else None
 
-def flair_sentiment(input:str):
-  sentence = Sentence(input)
-  tagger.predict(sentence)
 
-  if sentence and len(sentence.labels) >= 1 and sentence.labels[0]:
+def flair_sentiment(input:str):
+  if (len(input) > 0):
+    sentence = Sentence(input)
+    tagger.predict(sentence)
     return sentence.labels[0].value, sentence.labels[0].score 
   else:
     print("String is empty. No result.")
     return
 
+
+def vader_sentiment(input:str):
+  if input and len(input) > 0:
+    vs = analyzer.polarity_scores(input)
+    return vs['neg'], vs['neu'], vs['pos'], vs['compound']
+  else:
+    print("String is empty. No result.")
+    return 
